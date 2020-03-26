7f297fa26c89136423e23b1060e108e04e9acb37
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-26 15:17:16 -0600

    fix typo

--- lib/player_list.rb
+++ lib/player_list.rb
@@ -1,20 +1,20 @@
 require './lib/player'
 
 class PlayerList
-  @players_list = Hash.new(0)
+  @@players_list = Hash.new(0)
   def initialize(player)
-    @players_list[player.symbol] = player
+    @@players_list[player.symbol] = player
   end
 
   def add_player(player)
-    @players_list[player.symbol] = player
+    @@players_list[player.symbol] = player
   end
 
   def list_players
-    @players_list.each { |_k, v| p "Player =>#{v.name} Symbol =>#{v.symbol}" }
+    @@players_list.each { |_k, v| p "Player =>#{v.name} Symbol =>#{v.symbol}" }
   end
 
   def validate_unique_name(player)
-    @players_list['X'].name != player.name
+    @@players_list['X'].name != player.name
   end
 end
