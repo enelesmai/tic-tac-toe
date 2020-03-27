c0a6ceccddd2abf1fc5239b58719d847c3040801
enelesmai <xochitl.selene.fp@gmail.com>
2020-03-27 11:08:21 -0600

    changed validate_position method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require './lib/board'
 
 class Game
+  @@message
   def initialize; end
 
   def start
@@ -10,32 +11,31 @@ class Game
 
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Metrics/BlockNesting
-  def ask_for_position(player)
-    x = false
-    until x == true
-      print "Player '#{player.name}' please choose a position (from 1 to 9): "
-      position = gets.chomp.to_i
-
+  def validate_position(position, player)
+      x = false
       if position.is_a? Integer
         if position.positive? && position < 10
           if @board.position_taken(position) == false
             x = true
-			@board.update_board(position, player.symbol)
-			@board.display_board
+			      @board.update_board(position, player.symbol)
+			      @board.display_board
           else
-            puts 'The position is taken, choose another position'
+            @@message = 'The position is taken, choose another position'
           end
         else
-          puts 'Wrong position, choose a position between 1 to 9'
+          @@message = 'Wrong position, choose a position between 1 to 9'
         end
       else
-        puts 'Wrong position, choose a position between 1 to 9'
+        @@message = 'Wrong position, choose a position between 1 to 9'
       end
-    end
+      x
   end
-
   # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Metrics/BlockNesting
+
+  def self.get_message
+    return @@message
+  end
 
   def winner?(symbol)
     return @board.check_line_of_symbols(symbol)
